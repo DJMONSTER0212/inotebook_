af983c6d08920883cfc9ee4b2cae0f3efeed5365
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2023-03-10 01:00:11 +0530

    added a new endpoint for deleting note using delete method

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -39,31 +39,87 @@ router.post('/addnote', fetchUser, [
 
 })
 
-// route 3 : update an existing node using Post : "/api/notes/updatenode". Login required
+// route 3 : update an existing node using PUT: "/api/notes/updatenode". Login required
 
 router.put('/updatenote/:id', fetchUser, async (req, res) => {
-    const {title,description,tag} = req.body;
+    const { title, description, tag } = req.body;
+    try {
+        // create a newNote Object
+        const newNote = {};
+        if (title) {
+            newNote.title = title;
+        }
+        if (description) {
+            newNote.description = description;
+        }
+        if (tag) {
+            newNote.tag = tag;
+        }
+
+        // find  the node to be updated and Update it
+        let note = await Note.findById(req.params.id);
+        if (!note) { return res.status(404).send("Not found") };
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not allowed")
+        }
+
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })  // new true ka matlab yeah hai ki agar koi naya contact aata hai toh woh create ho jaayega basically.
+        res.json({ note });
+
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send("Internal Server error occured")
+    }
     // create a newNote Object
     const newNote = {};
-    if(title){
+    if (title) {
         newNote.title = title;
-    } 
-    if(description){
+    }
+    if (description) {
         newNote.description = description;
-    } 
-    if(tag){
+    }
+    if (tag) {
         newNote.tag = tag;
-    } 
+    }
 
     // find  the node to be updated and Update it
     let note = await Note.findById(req.params.id);
-    if(!note){return res.status(404).send("Not found")};
-    if(note.user.toString()!== req.user.id){
+    if (!note) { return res.status(404).send("Not found") };
+    if (note.user.toString() !== req.user.id) {
         return res.status(401).send("Not allowed")
     }
 
-    note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote},{new : true})  // new true ka matlab yeah hai ki agar koi naya contact aata hai toh woh create ho jaayega basically.
-    res.json({note});
+    note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })  // new true ka matlab yeah hai ki agar koi naya contact aata hai toh woh create ho jaayega basically.
+    res.json({ note });
+
+
+})
+
+
+// route 4 : delete an existing node using DELETE : "/api/notes/deletenote". Login required
+
+router.delete('/deletenote/:id', fetchUser, async (req, res) => {
+    const { title, description, tag } = req.body;
+    // verify karna hai ki joh note yeah insaan delete kar raha hai kya woh ushi kya hai
+    try {
+        let note = await Note.findById(req.params.id);
+        if (!note) { return res.status(404).send("Not found") };
+
+        // Allow Deletion only if user own this note
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not allowed")
+        }
+
+        note = await Note.findByIdAndDelete(req.params.id)
+        res.json({ "Success": "Note has been deleted ", note: note });
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send("Internal Server error occured")
+    }
+
+    // find  the node to be updated and deleted it
 
 
 })
